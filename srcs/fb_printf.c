19b5ddbad8396118c18282e140c661e5fa7c0859
FelBenini <felbenini@gmail.com>
2025-05-18 22:38:11 -0300

    %c and %s formats added

--- srcs/fb_printf.c
+++ srcs/fb_printf.c
@@ -1,13 +1,15 @@
 #include "../includes/fb_printf.h"
 
-void    fb_putchar(char c)
+int fb_putchar(char c)
 {
-        write(1, &c, 1);
+	write(1, &c, 1);
+	return (1);
 }
 
-void fb_putnbr(int nb)
+int fb_putnbr(int nb)
 {
 	long int num;
+	static int len = 0;
 
 	num = nb;
     if (num < 0) {
@@ -16,11 +18,32 @@ void fb_putnbr(int nb)
     }
     if (num >= 0 && num <= 9) {
 		fb_putchar(num + '0');
+		len += 1;
     }
 	if (num > 9) {
 		fb_putnbr(num / 10);
 		fb_putnbr(num % 10);
 	}
+	return (len);
+}
+
+int fb_putstr(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		fb_putchar(str[i++]);
+	return (i);
+}
+
+void print_formats(char c, va_list args, int *printed_chars)
+{
+	if (c == 'd')
+		*printed_chars += fb_putnbr(va_arg(args, int));
+	if (c == 'c')
+		*printed_chars += fb_putchar(va_arg(args, int));
+	if (c == 's')
+		*printed_chars += fb_putstr(va_arg(args, char *));
 }
 
 int fb_printf(char *str, ...)
@@ -28,15 +51,16 @@ int fb_printf(char *str, ...)
 	int i = 0;
 	va_list args;
 	va_start(args, str);
+	int printed_chars = 0;
 
 	while (str[i])
 	{
 		if (str[i] == '%') {
-			fb_putnbr(va_arg(args, int));
+			print_formats(str[i + 1], args, &printed_chars);
 			i += 2;
 		} else
-			fb_putchar(str[i++]);
+			printed_chars += fb_putchar(str[i++]);
 	}
 	va_end(args);
-	return (i);
+	return (printed_chars);
 }
